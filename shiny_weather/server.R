04e5e8db1a33031f2aec8b739de9470d31dd4a58
karafede <karafede@hotmail.com>
2016-05-31 20:32:10 +0100

    Add files via upload

--- shiny_weather/server.R
+++ shiny_weather/server.R
@@ -7,24 +7,70 @@ library(leaflet)
 
 shinyServer(function(input, output) {
 
-  finalMap <- reactive({
-    
- #   base <- input$variable
-    
-    # Create base map
-    map <- leaflet() %>%
+  finalMap_rain <- reactive({
+    map_rain <- leaflet() %>%
       addTiles() %>% 
-      setView(10, 44, 4) %>%
-      addProviderTiles(as.character(input$variable), options = providerTileOptions(opacity = 0.35))
-      
-    # Return
-    map
-    
+      setView(18, 38, 4) %>%
+     # addProviderTiles("Stamen.TonerBackground", options = providerTileOptions(opacity = 1)) %>%
+      addProviderTiles("OpenWeatherMap.Rain", options = providerTileOptions(opacity = 0.35))
+  })
+  
+  finalMap_temperature <- reactive({
+    map_temperature <- leaflet() %>%
+      addTiles() %>% 
+      setView(18, 38, 4) %>%
+      addProviderTiles("OpenWeatherMap.Temperature", options = providerTileOptions(opacity = 0.35))
+  })
+  
+  
+  finalMap_clouds <- reactive({
+    map_clouds <- leaflet() %>%
+    #  addTiles() %>% 
+      setView(18, 38, 4) %>%
+      addProviderTiles("Thunderforest.TransportDark", options = providerTileOptions(opacity = 1)) %>%
+      addProviderTiles("OpenWeatherMap.Clouds", options = providerTileOptions(opacity = 0.35))
+  })
+  
+  
+  finalMap_wind <- reactive({
+    map_wind <- leaflet() %>%
+      addTiles() %>% 
+      setView(18, 38, 4) %>%
+      addProviderTiles("OpenWeatherMap.Wind", options = providerTileOptions(opacity = 0.35))
+  })
+  
+  finalMap_pressure <- reactive({
+    map_pressure <- leaflet() %>%
+      addTiles() %>% 
+      setView(18, 38, 4) %>%
+      addProviderTiles("OpenWeatherMap.PressureContour", options = providerTileOptions(opacity = 0.35))
+  })
+  
+  finalMap_AOD <- reactive({
+    map_AOD <- leaflet() %>%
+      addTiles() %>% 
+      setView(18, 38, 4) %>%
+      addProviderTiles("NASAGIBS.ModisTerraAOD", options = providerTileOptions(opacity = 0.35))
+  })
+  
+  finalMap_Chlorophyll <- reactive({
+    map_Chlorophyll <- leaflet() %>%
+      addTiles() %>% 
+      setView(18, 38, 4) %>%
+      addProviderTiles("NASAGIBS.ModisTerraChlorophyll", options = providerTileOptions(opacity = 0.35))
   })
   
   # Return to client
-  output$myMap = renderLeaflet(finalMap())
+  output$myMap_rain = renderLeaflet(finalMap_rain())
+  output$myMap_temperature = renderLeaflet(finalMap_temperature())
+  output$myMap_clouds = renderLeaflet(finalMap_clouds())
+  output$myMap_wind = renderLeaflet(finalMap_wind())
+  output$myMap_pressure = renderLeaflet(finalMap_pressure())
+  output$myMap_AOD = renderLeaflet(finalMap_AOD())
+  output$myMap_Chlorophyll = renderLeaflet(finalMap_Chlorophyll())
   
 
+  
+  
 
 })
--- shiny_weather/ui.R
+++ shiny_weather/ui.R
@@ -7,38 +7,55 @@ library(shinydashboard)
 ############################################################
 
 
-
-shinyUI(
-  dashboardPage( skin = "yellow", 
-    dashboardHeader(title = "WeatherMaps"),
-    
-    dashboardSidebar(sidebarMenu(
-      menuItem("Dashboard", tabName = "MetData", icon = icon("dashboard")
-      )
-      
-    )),
-    
+ui <- dashboardPage(
+  skin = "yellow",
+  dashboardHeader(title = "EasyWeather"),
   
-  dashboardBody(
-      tabItem(tabName = "MetData",
-              fluidRow(
-                 box(height = 80, width = 7,
-                        selectInput("variable", "Met Parameter", c("Rain" = "OpenWeatherMap.Rain",
-                                                                   "Clouds" = "OpenWeatherMap.Clouds",
-                                                                   "Wind" = "OpenWeatherMap.Wind",
-                                                                   "Temperature" = "OpenWeatherMap.Temperature",
-                                                                   "Pressure" = "OpenWeatherMap.PressureContour",
-                                                                   "AOD" ="NASAGIBS.ModisTerraAOD",
-                                                                   "Chlorophyll" = "NASAGIBS.ModisTerraChlorophyll"))
-                     ),
+  dashboardSidebar(paste("Time:",Sys.time()),
+                   sidebarMenu(
+                     menuItem("Meteo", tabName = "Met", icon = icon("th")),
+                     menuItem("Aerosols", tabName = "MODIS", icon = icon("dashboard"))
+                   )),
+    
 
-                    
-                # Show leaflet map 
-                mainPanel(
-                  leafletOutput('myMap', height = 750, width = 950)
-                )
-      
+  dashboardBody(tabItems(
+    # First tab content
+    tabItem(tabName = "Met",
+            fluidRow(
+              tabBox(
+                height = 750, width = 950, selected = tags$b("Rain"),
+                #  title = h1("Weather Maps"),
+                tabPanel(
+                 tags$b("Rain"), leafletOutput('myMap_rain', height = 650, width = 750)
+                ),
+                tabPanel(
+                  tags$b("Temp."), leafletOutput('myMap_temperature', height = 650, width = 750)
+                ),
+                tabPanel(
+                  tags$b("Clouds"), leafletOutput('myMap_clouds', height = 650, width = 750)
+                ),
+                tabPanel(
+                  tags$b("Wind"), leafletOutput('myMap_wind', height = 650, width = 750
+                ))
+#                 tabPanel(
+#                   tags$b("Pressure"), leafletOutput('myMap_pressure', height = 650, width = 750)
+#                 )
               )
-            
-      ))
+            )),
+    
+    tabItem(tabName = "MODIS",
+            fluidRow(
+              tabBox(
+                height = 750, width = 950, selected = "AOD",
+                tabPanel("AOD", leafletOutput(
+                  'myMap_AOD', height = 650, width = 750
+                )),
+                tabPanel(
+                  "Chlorophyll", leafletOutput('myMap_Chlorophyll', height = 650, width = 750
+               ))
+              )
+            ))
   ))
+)
+          
+
